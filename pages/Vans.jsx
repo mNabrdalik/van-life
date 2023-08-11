7bac1e69b7e7c7ec264243754f7c3f1b16ca1898
Michał Nabrdalik <m.nabrdalik.93@gmail.com>
2023-08-11 20:54:17 +0200

    VanDetails subpage and params

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -1,8 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Vans() {
 
+    //state for data from memes api
+    const[vans, setVans] = React.useState([]);
+
+    //side effect - on first site render (empty array), save database from allVans state
+    React.useEffect(() => {
+        fetch("/api/vans")
+            .then(res => res.json())
+            .then(data => setVans(data.vans))    
+    },[])
+
+    const vanElements = vans.map(item => (
+        <Link to={`/vans/${item.id}`}>
+            <div key={item.id} className="van-tile">
+                <img src={item.imageUrl} alt={item.name} />
+                <div className="van-info">
+                    <h3>{item.name}</h3>
+                    <p>${item.price} <span>/day</span></p>
+                </div>
+                <i className={`van-type ${item.type} selected`}>{item.type}</i>
+            </div>
+        </Link>
+
+    ))
+
     return (
-        <h1>Vans page goes here </h1>
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
+
     )
 }

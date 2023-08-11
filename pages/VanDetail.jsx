d5e3fe33ebefeed4c65d277501ed15a5404052de
Michał Nabrdalik <m.nabrdalik.93@gmail.com>
2023-08-11 21:36:23 +0200

    update VanDetail

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
+import arrowBack from "../images/ArrowBack.png"
 
 export default function VanDetail() {
     //data from url -> :id
@@ -17,12 +18,13 @@ export default function VanDetail() {
         <div className="van-detail-container">
             {van ? (
             <div className="van-detail">
-                <img src={van.imageUrl} alt={van.name} />
+                <Link to={"/vans"} className="back-btn"><img src={arrowBack} alt="Back to Vans" /> <span>Back to all vans</span></Link>
+                <img className="van-img" src={van.imageUrl} alt={van.name} />
                 <i className={`van-type ${van.type} selected`}>{van.type}</i>
                 <h2>{van.name}</h2>
                 <p className="van-price"><span>${van.price}</span>/day</p>
                 <p>{van.description}</p>
-                <button className="link-button">Rent this van</button>
+                <button className="link-button btn btn-primary">Rent this van</button>
             </div>
             ) : <h2>Loading ...</h2> }
         </div>

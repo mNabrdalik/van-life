899ba4687a0dc833a903bba341f2ba905b5f4d99
Michał Nabrdalik <m.nabrdalik.93@gmail.com>
2023-08-20 21:42:28 +0200

    add api file and changed fetch data

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, useSearchParams } from "react-router-dom";
+import {getVans} from "../../api"
 
 export default function Vans() {
 
@@ -13,9 +14,12 @@ export default function Vans() {
 
     //side effect - on first site render (empty array), save database from allVans state
     React.useEffect(() => {
-        fetch("/api/vans")
-            .then(res => res.json())
-            .then(data => setVans(data.vans))    
+        async function loadVans() {
+            const data = await getVans()
+            setVans(data)
+        }
+
+        loadVans()
     },[])
 
     //check if was used search
